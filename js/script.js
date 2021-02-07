fd2ca172f938bf8893a95ebf5a987ac89d2b96e6
Shamim Reza <wpshamim@gmail.com>
2021-02-07 22:59:20 +0600

    update

--- js/script.js
+++ js/script.js
@@ -68,7 +68,10 @@ const getRecipes = (recipes) => {
     const searchCount = recipes.length
     const searchResult = document.getElementById('search-result')
     const searchValue = document.getElementById('search-input').value
-    const searchResultCount = `<p class="card-title">${searchCount} recipe results for "${searchValue}"</p>`
+    const searchResultCount = 
+    `<div class="card-body bg-light text-dark rounded mb-5">
+        <p class="card-title">${searchCount} recipe results for "${searchValue}"</p>
+    </div>`
     searchResult.innerHTML = searchResultCount
 
     // search input value default
